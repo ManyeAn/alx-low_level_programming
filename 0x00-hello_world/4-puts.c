47b7edc309eaf6e77778b9e08b1e4097aace09d3
Naa Ansaah Bruce-Tagoe <ansaahnaa@gmail.com>
2022-04-11 14:00:12 -0700

    Pick puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 /*
  * main - Entry point
  *
@@ -7,5 +7,6 @@
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
+		
 		return (0);
 }
