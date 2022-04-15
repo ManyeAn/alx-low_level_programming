90f8314fd3e58ee920e272077899651998eca5fa
Naa Ansaah Bruce-Tagoe <ansaahnaa@gmail.com>
2022-04-14 17:39:32 -0700

    Ni

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: 0 
+ * Return: 0
  */
 
 int main(void)
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /*
- * while (i < 10)
- * {putchar(i);
- * }
+ *while (i < 10)
+ *{putchar(i);
+ *}
  */
-printf("Infinite loop avoided! //o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
