8f2cba1e41fc6c3e6f831937e17d230a48e797b0
Naa Ansaah Bruce-Tagoe <ansaahnaa@gmail.com>
2022-04-15 14:24:18 -0700

    Funcs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include"main.h"
 
 /**
  * print_alphabet_x10 - prints alphabet 10 times
@@ -13,6 +13,7 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 	c = 'a';
+
 	while (c <= 'z')
 	{
 	_putchar(c);
